dd676e25d823eefd094f465da265ffd18f193254
saify96 <mohammad.alsaify@gmail.com>
2021-08-30 14:15:57 +0300

    branch for lab27

---
+++ app/src/main/java/com/example/taskmaster/SettingPage.java
@@ -0,0 +1,14 @@
+package com.example.taskmaster;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_page);
+    }
+}
---
+++ app/src/main/java/com/example/taskmaster/TaskDetail.java
@@ -0,0 +1,27 @@
+package com.example.taskmaster;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TaskDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_detail);
+
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToMainActivity = new Intent(TaskDetail.this, MainActivity.class);
+                startActivity(goToMainActivity);
+            }
+        });
+
+    }
+}
